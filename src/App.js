6d2f16f636d44cf7b0e95152401ef5f9f0559384
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-02-27 10:22:49 -0500

    creates DisplayLyrics component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Genius from './utility/Genius';
+import mockLyricData from './mock/mockLyricData';
+import DisplayLyrics from './components/DisplayLyrics/DisplayLyrics';
 import './App.css';
 
 function App() {
   const [userInput, setUserInput] = useState('');
-  const [songId, setSongId] = useState(null);
-
-  // * Test call to find song Id
+  const [songId, setSongId] = useState('');
+  const [lyrics, setLyrics] = useState('');
+  
+  // * Sets lyrics state * currently mocked
+    useEffect(() => {
+      setLyrics(mockLyricData());
+    }, [lyrics])
+  
+    // * Test call to find song Id
   /*const testfunc = async function() {
     const test = await Genius.search('homesick and noah');
     console.log(Genius.returnId(test.response.hits[1]));
   }
   testfunc()*/
 
+  
+
+
+  return (
+    <>
+      <DisplayLyrics lyrics={lyrics}/>
+    </>
+  )
   
   
 }
---
+++ src/components/DisplayLyrics/DisplayLyrics.js
@@ -0,0 +1,13 @@
+import React from 'react';
+  
+  function DisplayLyrics(props) {
+    const lyricsLineBreaks = props.lyrics.replaceAll('\n', '<br/>')
+    console.log(lyricsLineBreaks)
+
+    return (
+	  <div dangerouslySetInnerHTML={{__html: lyricsLineBreaks}} />
+	  );
+  }
+  
+  export default DisplayLyrics;
+  
