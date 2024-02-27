1471638ba309f8d05ed0d2a207a1e49491222bff
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-02-27 03:32:19 -0500

    adds test code to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Genius from './utility/Genius';
 import './App.css';
 
 function App() {
- 
+  const [userInput, setUserInput] = useState('');
+  const [songId, setSongId] = useState(null);
+
+  // * Test call to find song Id
+  /*const testfunc = async function() {
+    const test = await Genius.search('homesick and noah');
+    console.log(Genius.returnId(test.response.hits[1]));
+  }
+  testfunc()*/
+
+
+  
 }
 
 export default App;
